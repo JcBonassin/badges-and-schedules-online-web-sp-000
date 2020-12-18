19b740f4c75283e57f7f6e6f76c03d5afec6f6cb
JC <bonassin@gmail.com>
2020-12-18 21:06:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
 def badge_maker(names)
   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  names.each {|names| puts "Hello, my name is #{names}."}
+  names.each {|name| puts "Hello, my name is #{name}."}
 end
